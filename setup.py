581e939538f6f8e303c09dccff417806bcc5d0f6
Dipan Nanda <dipangithub@gmail.com>
2023-01-03 14:37:33 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    LONG_DESCRIPTION = "\n" + fh.read()
+
+VERSION = "1.0.0"
+DESCRIPTION = "Replicate ClickUp Database using Excel."
+
+# Setting up
+setup(
+    name="clickuptoexcel",
+    version=VERSION,
+    author="Dipan Nanda",
+    author_email="d19cyber@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=[
+        "python",
+        "ClickUp",
+        "Excel",
+        "pandas"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
